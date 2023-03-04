8ff4e6cc217ced8cf9d4d067b05233098b112287
Ammar Ali <108199986+WalidAmmarAli@users.noreply.github.com>
2023-03-04 01:45:57 +0000

    add boostrap to project add a for loop to show a paython object

--- main.py
+++ main.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask('app')
 
+JOBS = [
+  {
+    'id': 1,
+    'title': "Data Analaist",
+    'location': "Managua, Nicaragua",
+    'salary': "$600"
+  },
+  {
+    'id': 2,
+    'title': "Data Scientist",
+    'location': "Remote",
+    'salary': "$600"
+  },
+  {
+    'id': 3,
+    'title': "Backend development",
+    'location': "Managua, Nicaragua",
+    'salary': "$600"
+  },
+]
+
 
 @app.route('/')
 def hello_world():
-  return render_template('index.html')
+  return render_template('index.html', jobs=JOBS, company_name="DLS")
 
 
 if __name__ == "__main__":
